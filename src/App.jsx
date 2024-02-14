1bfdc37a2230c7ffcbea4c4559f7fb2b01213c0e
Tooyyib <adebisitooyyib@gmail.com>
2024-02-14 11:39:09 +0100

    setup development

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="bg-black h-screen flex justify-center items-center">
+    <div>
+    <h1 className="text-blue-300 fonts-bold text-5xl">WELCOME TO ZULAYKHA GLOBAL ACADEMY</h1>
+    <p className="text-white text-center font-bold text-3xl">deployment under process....</p>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default App
